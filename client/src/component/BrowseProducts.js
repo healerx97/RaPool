cf154c56708e77a17454ae79ffd2715398b55666
Kenny Yoon <ghun.yoon@gmail.com>
2021-08-27 22:54:39 -0400

    api key

--- client/src/component/BrowseProducts.js
+++ client/src/component/BrowseProducts.js
@@ -32,12 +32,10 @@ function BrowseProducts({createProduct, user, getRaffles}) {
         const res = await fetch(`https://amazon-product-reviews-keywords.p.rapidapi.com/product/search?keyword=${searchTerm}&country=US&category=aps`, {
                 "method": "GET",
                 "headers": {
-                    "x-rapidapi-key": REACT_APP_AMAZON_API_KEY,
-                    "x-rapidapi-host": "amazon-product-reviews-keywords.p.rapidapi.com",
-                    'X-Requested-With': 'XMLHttpRequest'
+                    "x-rapidapi-key": process.env.REACT_APP_AMAZON_API_KEY,
+                    "x-rapidapi-host": "amazon-product-reviews-keywords.p.rapidapi.com"
 
-                },
-                'credentials': 'include'
+                }
             })
             if (res.ok) {
                 console.log("finally")
